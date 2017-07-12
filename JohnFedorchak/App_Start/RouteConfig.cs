167db311d546b04bae7e39ab483173a03c774b2e
John Fedorchak <john.fedorchak@gmail.com>
2017-07-11 22:25:58 -0400

    Removed Home from URL.

--- JohnFedorchak/App_Start/RouteConfig.cs
+++ JohnFedorchak/App_Start/RouteConfig.cs
@@ -7,6 +7,12 @@ namespace JohnFedorchak {
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
 			routes.MapRoute(
+				"OnlyAction",
+				"{action}",
+				new { controller = "Home", action = "Index" }
+			);
+
+			routes.MapRoute(
 				"Default",
 				"{controller}/{action}/{id}",
 				new {controller = "Home", action = "Index", id = UrlParameter.Optional}
