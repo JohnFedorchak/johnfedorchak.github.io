06a15e413d320b13b6791ead7da3473d9defbce9
John Fedorchak <john.fedorchak@gmail.com>
2017-07-11 21:41:10 -0400

    Emptied out pages. Added email address.

--- JohnFedorchak/Controllers/HomeController.cs
+++ JohnFedorchak/Controllers/HomeController.cs
@@ -1,29 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
-namespace JohnFedorchak.Controllers
-{
-	public class HomeController : Controller
-	{
-		public ActionResult Index()
-		{
+namespace JohnFedorchak.Controllers {
+	public class HomeController : Controller {
+		public ActionResult Index() {
 			return View();
 		}
 
-		public ActionResult About()
-		{
-			ViewBag.Message = "Your application description page.";
-
+		public ActionResult About() {
 			return View();
 		}
 
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Your contact page.";
-
+		public ActionResult Contact() {
 			return View();
 		}
 	}
